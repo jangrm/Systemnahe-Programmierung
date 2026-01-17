7db892756df9a5265f9a460839780c779fca0697
Nino Beck <mail@n-beck.de>
2026-01-17 13:22:53 +0100

    add predicate function pointer type

--- sopfind/src/filter.h
+++ sopfind/src/filter.h
@@ -1,6 +1,10 @@
 #ifndef FILTER_H
 #define FILTER_H
 
+#include <sys/stat.h>
+
+typedef int (*PredicateFunc)(const char *path, const struct stat *st, void *data);
+
 typedef struct FilterNode {
     char *data;
     struct FilterNode *next;
